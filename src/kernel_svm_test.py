f5eca1d402c4f84fd6c55fbd998925ec18362efb
Joshua Kirstein <joshkirstein@Joshuas-MacBook-Pro.local>
2016-04-26 15:13:42 -0400

    Added another visualization.

--- src/kernel_svm_test.py
+++ src/kernel_svm_test.py
@@ -135,8 +135,65 @@ def test_visualization():
 	plt.axis('equal')
 	plt.show()
 
-# Example test. Run in main file :)
+def test_visualization2():
+	mean1 = [1, 1]
+	cov1 = [[7, 0], [0, 7]]
+	mean2 = [1, 1]
+	cov2 = [[0.35, 0], [0, 0.35]]
+	p1 = 150
+	p2 = 50
+	x1, y1 = np.random.multivariate_normal(mean1, cov1, p1).T
+	x2, y2 = np.random.multivariate_normal(mean2, cov2, p2).T
+
+	xs = np.zeros( (p1+p2, 2) )
+	ys = np.zeros( (p1+p2, 1) )
+	for i in range(p1):
+		xs[i,0] = x1[i]
+		xs[i,1] = y1[i]
+		ys[i,0] = 1.0
+
+	for i in range(p2):
+		xs[i+p1,0] = x2[i]
+		xs[i+p1,1] = y2[i]
+		ys[i+p1,0] = -1.0
+
+	N = p1+p2
+	K = 2.0
+	C = 20.0
+
+	svm = kernel_svm.kernel_svm(xs, ys, N, K, C, kernel_svm.gauss_kernel)
+
+	samp_x = []
+	samp_y = []
+
+	samp_x2 = []
+	samp_y2 = []
+
+	cut = 100
+
+	startx = -5.0
+	endx = 5.0
+
+	starty = -4.0
+	endy = 5.0
+
+	for i in range(cut):
+		x = startx + (endx-startx)/(cut-1) * i
+		for j in range(cut):
+			y = starty + (endy-starty)/(cut-1) * j
+			clazz = svm.classify(np.array([[x, y]]))
+			if (clazz < 0.05) and (clazz > -0.05):
+				samp_x.append(x)
+				samp_y.append(y)
+
+
+	plt.plot(x1, y1, 'g^', x2, y2, 'bs', np.array(samp_x), np.array(samp_y), 'r^')
+	plt.axis('equal')
+	plt.show()
+
+# Shows visualizations
 if __name__ == "__main__":
-	datasets = ['breast_cancer', 'digits', 'forest_mapping']
+	#datasets = ['breast_cancer', 'digits', 'forest_mapping']
 	#test(datasets[0])
 	test_visualization()
+	test_visualization2()
